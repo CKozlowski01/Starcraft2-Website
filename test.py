56fd225b6591ed8332957e91dc3aed6e434ee644
TheCeleryK <cezarykozlowski13@gmail.com>
2022-05-19 16:35:48 +0100

    deployment

--- test.py
+++
@@ -1,8 +0,0 @@
-import spawningtool
-from spawningtool.parser import GameTimeline
-
-class SpawningReplayModel():
- 
-  def loadRequestedReplay(self, filePath):
-    result_replay = spawningtool.parser.parse_replay(filePath)
-    return result_replay
